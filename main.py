67c08de6e69a6ad045d39ebe23eb0dfcbac3a765
Aayuraj3006 <ashutosh981209@gmail.com>
2026-05-07 14:53:08 +0530

    Eel connection

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import os
+import eel
+
+eel.init("www")
+
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+eel.start(
+    'index.html',
+    mode=None,
+    host='localhost',
+    port=8000,
+    block=True
+)
